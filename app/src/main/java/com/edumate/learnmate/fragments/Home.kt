fe3a47be198ae4f892cea4f0e29f90c7d599ac3c
Princekumar80107 <princekumar.80107@gmail.com>
2024-04-18 20:55:07 +0530

    Option Toolbar is Added

--- app/src/main/java/com/edumate/learnmate/fragments/Home.kt
+++ app/src/main/java/com/edumate/learnmate/fragments/Home.kt
@@ -2,13 +2,20 @@ package com.edumate.learnmate.fragments
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.PorterDuff
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -17,6 +24,7 @@ import com.denzcoskun.imageslider.constants.ScaleTypes
 import com.denzcoskun.imageslider.models.SlideModel
 import com.edumate.learnmate.R
 import com.edumate.learnmate.activities.CodingPlaylistActivity
+import com.edumate.learnmate.activities.ForgetPasswordActivity
 import com.edumate.learnmate.activities.PlacementSeriesActivity
 import com.edumate.learnmate.activities.StudyPlaylistActivity
 import com.edumate.learnmate.adapters.DepartmentAdapter
@@ -29,6 +37,7 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.firestore.FirebaseFirestore
+
 
 class Home : Fragment() {
 
@@ -60,10 +69,10 @@ class Home : Fragment() {
 
     private fun setUpViews(view: View) {
         if (uid.isNotEmpty()) { // Check if the user ID is not empty
-            getUserData(view) // Call method to fetch user data
+            setUpToolbar(view) // Call method to fetch user data
         }
         setUpImageSlide(view)
-        setUpDepartment(view)
+        setUpDepartment()
         setUpDepartmentRecyclerview(view)
         setUpStudyPlaylist(view)
         setUpCodingPlaylist(view)
@@ -101,51 +110,113 @@ class Home : Fragment() {
         } else {
             // User details are already available in SharedPreferences, no need to fetch from Firebase
         }
-
     }
 
-    private fun getUserData(view: View) {
-        val name =
-            view.findViewById<com.google.android.material.textview.MaterialTextView>(R.id.userName) // Find the TextView for displaying user's name
+    private fun setUpToolbar(view: View) {
+        val homeToolbar =
+            view.findViewById<androidx.appcompat.widget.Toolbar>(R.id.homeToolbar) // Find the TextView for displaying user's name
 
+        homeToolbar.title = ""
+        (activity as AppCompatActivity?)!!.setSupportActionBar(homeToolbar)
+
+        val titleToolbarTxt =
+            view.findViewById<com.google.android.material.textview.MaterialTextView>(R.id.titleToolbarTxt)
         val userName = getUserName()
         val index = userName?.indexOf(' ')
 
         if (index != -1 && userName != null) {
-            name.text = "Hello " + index?.let { userName.substring(0, it) } + "!"
-        } else if (userName != null){
-            name.text = "Hello " + userName + "!"
+            titleToolbarTxt.text = "Hello " + index?.let { userName.substring(0, it) } + "!"
+        } else if (userName != null) {
+            titleToolbarTxt.text = "Hello " + userName + "!"
         }
+
+        context?.let { ContextCompat.getColor(it, R.color.black1) }
+            ?.let { homeToolbar.overflowIcon?.setColorFilter(it, PorterDuff.Mode.SRC_ATOP) }
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.option_menu, menu)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.contactUs -> {
+                val intent = Intent(Intent.ACTION_VIEW)
+                val data =
+                    Uri.parse("mailto:edumate.contact@gmail.com?subject=Want to Resolve Query &body=")
+                intent.data = data
+                startActivity(intent)
+                return true
+            }
+
+            R.id.share -> {
+                val url = "https://play.google.com/store/apps/details?id=com.edumate.learnmate"
+                val intent = Intent(Intent.ACTION_SEND)
+                val sub = "EDUMATE: Your Learning Partner"
+                val body = "Discover the magic of Edumate! Say goodbye to study stress with our easy-to-use platform. Dive into structured notes, engaging placement series, curated YouTube playlists, and thrilling coding sessions. Experience the joy of learning with Edumate's user-friendly interface!"
+
+                intent.type = "text/html"
+                intent.putExtra(Intent.EXTRA_SUBJECT, sub)
+                intent.putExtra(Intent.EXTRA_TEXT, "$sub\n\n$body\n\n$url")
+                startActivity(Intent.createChooser(intent, "Share Via"))
+                return true
+            }
+
+            R.id.rateUs -> {
+                val url =
+                    "https://play.google.com/store/apps/details?id=com.edumate.learnmate"
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                startActivity(intent)
+                return true
+            }
+
+            R.id.forgetPassword -> {
+                val intent = Intent(activity, ForgetPasswordActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
     private fun getUserName(): String? {
-        val sharedPreferences = context?.getSharedPreferences("User_Details", Context.MODE_PRIVATE)
+        val sharedPreferences =
+            context?.getSharedPreferences("User_Details", Context.MODE_PRIVATE)
         return sharedPreferences?.getString("user_name", null)
     }
 
     private fun setUpPlacementSeries(view: View) {
         // Set up onClickListener for navigating to PlacementSeriesActivity
-        val listViewArrow = view.findViewById<ImageView>(R.id.placementArrow)
+        val placementSeries = view.findViewById<androidx.constraintlayout.widget.ConstraintLayout>(R.id.placementSeries)
 
-        listViewArrow?.setOnClickListener {
+        placementSeries?.setOnClickListener {
             val intent = Intent(activity, PlacementSeriesActivity::class.java)
             startActivity(intent)
         }
     }
 
     private fun setUpCodingPlaylist(view: View) {
-        val listViewArrow = view.findViewById<ImageView>(R.id.codingPlaylistArrow)
+        val codingPlaylist = view.findViewById<androidx.constraintlayout.widget.ConstraintLayout>(R.id.codingPlaylist)
 
-        listViewArrow?.setOnClickListener {
+        codingPlaylist?.setOnClickListener {
             val intent = Intent(activity, CodingPlaylistActivity::class.java)
             startActivity(intent)
         }
     }
 
     private fun setUpStudyPlaylist(view: View) {
-        val listViewArrow = view.findViewById<ImageView>(R.id.studyPlaylistArrow)
+        val studyPlaylist = view.findViewById<androidx.constraintlayout.widget.ConstraintLayout>(R.id.studyPlaylist)
 
-        listViewArrow?.setOnClickListener {
+        studyPlaylist?.setOnClickListener {
             val intent = Intent(activity, StudyPlaylistActivity::class.java)
             startActivity(intent)
         }
@@ -153,13 +224,14 @@ class Home : Fragment() {
 
     private fun setUpDepartmentRecyclerview(view: View) {
         // Set up RecyclerView for displaying departments
-        val departmentRecyclerView = view.findViewById<RecyclerView>(R.id.departmentRecyclerView)
+        val departmentRecyclerView =
+            view.findViewById<RecyclerView>(R.id.departmentRecyclerView)
         departmentAdapter = context?.let { DepartmentAdapter(it, departmentList) }!!
         departmentRecyclerView?.layoutManager = GridLayoutManager(context, 3)
         departmentRecyclerView?.adapter = departmentAdapter
     }
 
-    private fun setUpDepartment(view: View) {
+    private fun setUpDepartment() {
         // Fetch departments data from Firestore and populate the list
         val collectionReference = firestore.collection("Departments List")
         collectionReference.addSnapshotListener { value, error ->
